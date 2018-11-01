343c629271d167e26c257a352ba9f82c8662da71
Javier S. A <jsantander@outlook.com>
2018-11-01 00:52:49 -0300

    Add config.js for environment setup

---
+++ config.js
@@ -0,0 +1,20 @@
+// simple setup
+// configuration object
+const environments = {};
+
+
+//set the port and name
+environments.development = {
+    port:3000,
+    envName:"development"
+}
+
+//Get the requested env
+const currentEnvironment =  typeof( process.env.NODE_ENV) == "string" ? process.env.NODE_ENV.toLowerCase() : "";
+
+//Pick the default in any case
+const environmentToExport = typeof( environments[currentEnvironment]) == "object" ? environments[currentEnvironment] : environments.development;
+
+//export the configuration object
+module.exports = environmentToExport;
+
